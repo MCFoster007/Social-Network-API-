ed5fd94dfb92dec00a632fcf72e940854d4b6ad6
mcfoster007 <mariechantalfoster@gmail.com>
2024-11-25 19:56:12 -0500

    added code to models

--- src/routes/api/userRoutes.ts
+++ src/routes/api/userRoutes.ts
@@ -28,21 +28,3 @@ router.delete('/:id', (req, res)=>{
 
 modules.export = router
 
-const { 
-  getAllUsers, 
-  getUserById, 
-  createUser, 
-  updateUser, 
-  deleteUser 
-} = require('../controllers/userController');
-
-router.route('/')
-  .get(getAllUsers)
-  .post(createUser);
-
-router.route('/:userId') // use id to :userId
-  .get(getUserById)
-  .put(updateUser)
-  .delete(deleteUser);
-
-module.exports = router;
